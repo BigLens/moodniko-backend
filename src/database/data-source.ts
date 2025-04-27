8f10652d8cd88819f6467621ad751d08cdc67b3e
Biglens <adedejibioku@gmail.com>
2025-04-27 15:06:25 +0100

    database connection error fixed

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,7 +8,7 @@ const dataSource = new DataSource({
     type: (process.env.DB_TYPE as any) || 'postgres',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT!,
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: false,
