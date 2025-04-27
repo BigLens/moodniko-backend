47c45dc8d245e6029440ecea14612224b82a7430
Biglens <adedejibioku@gmail.com>
2025-04-27 14:19:24 +0100

    typeorm and config module registered in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dataSource from './database/data-source';
 
 @Module({
-  imports: []
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+}),
+
+TypeOrmModule.forRootAsync({
+  useFactory: async () => ({
+  ...dataSource.options
+  }),
+  dataSourceFactory: async () => {
+    if (!dataSource.isInitialized) {
+      await dataSource.initialize();
+    }
+    return dataSource;
+  }
+})
+]
 })
 export class AppModule {}
