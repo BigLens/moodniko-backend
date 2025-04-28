f2beb6751f8b1e7a42c26a671bc5651af09b307b
Biglens <adedejibioku@gmail.com>
2025-04-28 15:16:06 +0100

    helper file for camel to snake naming convention implemented

---
+++ src/utils/snake_snake.ts
@@ -0,0 +1,26 @@
+import { NamingStrategyInterface, DefaultNamingStrategy } from 'typeorm';
+import { camelToSnakeCase } from './utils';
+
+export class SnakeNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
+
+  columnName(propertyName: string, customName: string | undefined, embeddedPrefixes: string[]): string {
+    return camelToSnakeCase(customName ? customName : propertyName);
+  }
+
+  
+  tableName(targetName: string, userSpecifiedName: string | undefined): string {
+    return camelToSnakeCase(userSpecifiedName ? userSpecifiedName : targetName);
+  }
+
+  joinColumnName(propertyName: string, referencedTableName: string): string {
+    return camelToSnakeCase(propertyName) + "_id";
+  }
+
+  joinTableName(firstTableName: string, secondTableName: string): string {
+    return camelToSnakeCase(firstTableName) + "_" + camelToSnakeCase(secondTableName);
+  }
+  
+  relationName(propertyName: string): string {
+    return camelToSnakeCase(propertyName);
+  }
+}
