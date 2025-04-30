ab91638272e7d0e7cddc715f179bc882ec123b17
Biglens <adedejibioku@gmail.com>
2025-04-30 18:31:17 +0100

    contents entity structured

---
+++ src/modules/contents/model/content.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from "@entities/base-entity";
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class ContentEntity extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    externalId: number;
+
+    @Column()
+    title: string;
+
+    @Column({ nullable: true })
+    description: string;
+
+    @Column({ nullable: true })
+    type: 'movie' | 'music' | 'podcast'
+
+    @Column({ nullable: true })
+    moodtag: string;  
+
+    @Column({ nullable: true })
+    imageUrl: string;
+}
