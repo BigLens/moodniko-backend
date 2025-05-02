692277c2c0cdcba38ec336aeb46e0ee3993e9075
Biglens <adedejibioku@gmail.com>
2025-05-02 23:17:56 +0100

    logger added to the application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT ?? 4002);
+  app.enableCors();
+
+  const env = process.env.NODE_ENV || 'development';
+  const port = process.env.PORT || 4002;
+
+  app.listen(port);
+
+  logger.log(`Server is running on http://localhost:${port} in ${env} mode`);
 }
 bootstrap();
