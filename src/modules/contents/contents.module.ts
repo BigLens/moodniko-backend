8e5eb90ecb388888ef273eab7bea802d0d29d6d0
Biglens <adedejibioku@gmail.com>
2025-05-03 15:47:59 +0100

    movie module removed from contents module

--- src/modules/contents/contents.module.ts
+++ src/modules/contents/contents.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContentsService } from '@modules/contents/contents.service';
 import { ContentsController } from '@modules/contents/contents.controller';
-import { MoviesModule } from '@modules/contents/movies/movies.module';
 
 @Module({
   providers: [ContentsService],
   controllers: [ContentsController],
-  imports: [MoviesModule],
 })
 export class ContentsModule {}
