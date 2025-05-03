7027b8157bb7550ea022e6b12d4122757a4bea78
Biglens <adedejibioku@gmail.com>
2025-05-03 15:11:03 +0100

    contents service registered in contents module

--- src/modules/contents/contents.module.ts
+++ src/modules/contents/contents.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 import { ContentsController } from './contents.controller';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   providers: [ContentsService],
   controllers: [ContentsController],
+  imports: [MoviesModule],
 })
 export class ContentsModule {}
