5eac9d562780b85394039f1b30a88ecfa33c3d4b
Biglens <adedejibioku@gmail.com>
2025-05-03 17:25:24 +0100

    migration created for content entity

--- src/database/migrations/1746287996349-createContentsTable.ts
+++ src/database/migrations/1746287996349-createContentsTable.ts
@@ -1,12 +1,11 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateContentsTable1746287996349 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TYPE "content_type_enum" AS ENUM('movie', 'music', 'podcast', 'book')
             `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "contents" (
                 "id" SERIAL PRIMARY KEY,
                 "external_id" VARCHAR NOT NULL,
@@ -19,14 +18,14 @@ export class CreateContentsTable1746287996349 implements MigrationInterface {
                 "updated_at" TIMESTAMP DEFAULT now()
                 )
             `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             DROP TABLE "contents"
             `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "content_type_enum"
             `);
-    }
+  }
 }
