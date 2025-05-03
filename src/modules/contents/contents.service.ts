b8faa17afb11fd7ab20a0911b14046a2419feb75
Biglens <adedejibioku@gmail.com>
2025-05-03 15:08:47 +0100

    contents service logic implemented to serve related files

--- src/modules/contents/contents.service.ts
+++ src/modules/contents/contents.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ContentType } from './enum/content.enum';
+import { ContentEntity } from './model/content.entity';
 
 @Injectable()
-export class ContentsService {}
+export class ContentsService {
+  constructor() {}
+
+  async getContentsByMood(
+    mood: string,
+    type: ContentType,
+  ): Promise<ContentEntity[]> {
+    if (type === ContentType.MOVIE) {
+      return [];
+    } else if (type === ContentType.MUSIC || type === ContentType.PODCAST) {
+      return [];
+    } else if (type === ContentType.BOOK) {
+      return [];
+    } else throw new Error(`Invalid content type: ${type}`);
+  }
+}
