2b8ff56f0114df9f9865cd41efb08a304bcfd5b7
Biglens <adedejibioku@gmail.com>
2025-05-14 00:24:30 +0100

    interface added for music and podcast

---
+++ src/modules/contents/providers/spotify/interface/spotify.interface.ts
@@ -0,0 +1,56 @@
+export interface SpotifyTrack {
+  id: string;
+  name: string;
+  artists: {
+    id: string;
+    name: string;
+  }[];
+  album: {
+    id: string;
+    name: string;
+    images: {
+      url: string;
+      height: number;
+      width: number;
+    }[];
+  };
+  external_urls: {
+    spotify: string;
+  };
+  preview_url: string | null;
+  popularity: number;
+  duration_ms: number;
+  explicit: boolean;
+}
+
+export interface SpotifyPodcast {
+  id: string;
+  name: string;
+  publisher: string;
+  description: string;
+  images: {
+    url: string;
+    height: number;
+    width: number;
+  }[];
+  external_urls: {
+    spotify: string;
+  };
+  total_episodes: number;
+  media_type: string;
+}
+
+export interface SpotifyAuthResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+}
+
+export interface SpotifySearchResponse {
+  tracks?: {
+    items: SpotifyTrack[];
+  };
+  shows?: {
+    items: SpotifyPodcast[];
+  };
+}
