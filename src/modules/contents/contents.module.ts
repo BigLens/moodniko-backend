13b99a2d096169a188ae7dec0048f9ddd51802dc
Biglens <adedejibioku@gmail.com>
2025-05-16 19:47:26 +0100

    contents service and module import route error fixed

--- src/modules/contents/contents.module.ts
+++ src/modules/contents/contents.module.ts
@@ -3,7 +3,7 @@ import { ContentsService } from '@modules/contents/contents.service';
 import { ContentsController } from '@modules/contents/contents.controller';
 import { MoviesModule } from '@modules/contents/providers/movies/movies.module';
 import { SpotifyModule } from '@modules/contents/providers/spotify/spotify.module';
-import { BooksModule } from '@modules/contents/providers/books/books.module';
+import { BooksModule } from '../contents/providers/books/books.module';
 
 @Module({
   imports: [MoviesModule, SpotifyModule, BooksModule],
