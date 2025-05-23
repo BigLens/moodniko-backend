e198e6e0cc27a2f0215680b2bce242b45aa0f6a3
Biglens <adedejibioku@gmail.com>
2025-05-23 19:21:47 +0100

    feat(controller): add 'type' query parameter for contents filtering

--- src/modules/contents/providers/spotify/spotify.controller.ts
+++ src/modules/contents/providers/spotify/spotify.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SpotifyService } from './spotify.service';
 import { ContentEntity } from '@modules/contents/model/content.entity';
+import { ContentType } from '@modules/contents/enum/content.enum';
+import { ApiSpotifyContent } from './docs/spotify.swagger';
+import { mapContentTypeToSpotifyType } from './mappers/spotify-type.mapper';
 
 @Controller('spotify')
 export class SpotifyController {
   constructor(private readonly spotifyService: SpotifyService) {}
 
   @Get('mood')
+  @ApiSpotifyContent()
   async getContentByMood(
     @Query('mood') mood: string,
+    @Query('contentType') type?: ContentType,
   ): Promise<ContentEntity[]> {
-    return this.spotifyService.fetchContentByMood(mood);
+    const spotifyContentType = mapContentTypeToSpotifyType(type);
+    return await this.spotifyService.fetchContentByMood(
+      mood,
+      spotifyContentType,
+    );
   }
 }
