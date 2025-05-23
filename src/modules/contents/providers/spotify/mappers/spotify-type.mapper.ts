c3fc8baf9ac5675277caf81434104fa8ce9093e0
Biglens <adedejibioku@gmail.com>
2025-05-23 19:34:06 +0100

    refactor: extract methods to different files for separation of concerns

---
+++ src/modules/contents/providers/spotify/mappers/spotify-type.mapper.ts
@@ -0,0 +1,20 @@
+import { ContentType } from '@modules/contents/enum/content.enum';
+import { SpotifyContentType } from '../enum/spotify-content.enum';
+
+export const mapContentTypeToSpotifyType = (
+  type?: ContentType,
+): SpotifyContentType | undefined => {
+  if (!type) {
+    return undefined;
+  }
+
+  if (type === ContentType.MUSIC) {
+    return SpotifyContentType.MUSIC;
+  }
+
+  if (type === ContentType.PODCAST) {
+    return SpotifyContentType.PODCAST;
+  }
+
+  return SpotifyContentType.BOTH;
+};
