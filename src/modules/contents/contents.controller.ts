c9e7470f6479996fb7daf83823aade4e0f979d7c
Biglens <adedejibioku@gmail.com>
2025-05-26 18:03:53 +0100

    feature: add query param to contents controller to query contents by mood and type

--- src/modules/contents/contents.controller.ts
+++ src/modules/contents/contents.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ContentsService } from './contents.service';
+import { GetContentQueryDto } from './dto/get-content-query.dto';
+import { ContentEntity } from './model/content.entity';
+import { GetContentDoc } from './docs/get-content.doc';
 
+@ApiTags('contents')
 @Controller('contents')
-export class ContentsController {}
+export class ContentsController {
+  constructor(private readonly contentsService: ContentsService) {}
+
+  @Get()
+  @GetContentDoc()
+  async getContentByMood(
+    @Query(new ValidationPipe({ transform: true }))
+    query: GetContentQueryDto,
+  ): Promise<ContentEntity[]> {
+    return this.contentsService.getContentsByMood(query.mood, query.type);
+  }
+}
