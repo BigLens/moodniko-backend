3f4bc4dde64e3d9005331dd2d7e379ae313defae
Biglens <adedejibioku@gmail.com>
2025-07-22 17:18:03 +0100

    feat: add entity to implement user-to-saved contents

--- src/modules/contents/save_contents/save-content.entity.ts
+++ src/modules/contents/save_contents/save-content.entity.ts
@@ -8,18 +8,24 @@ import {
   Index,
 } from 'typeorm';
 import { ContentEntity } from '../model/content.entity';
+import { UserEntity } from '@modules/user/entity/user.entity';
 
 @Entity('saved_contents')
 @Index('IDX_SAVED_CONTENTS_CONTENT_ID', ['contentId'])
 @Index('IDX_SAVED_CONTENTS_MOOD', ['mood'])
 @Index('IDX_SAVED_CONTENTS_CREATED_AT', ['createdAt'])
-@Index('IDX_SAVED_CONTENTS_UNIQUE', ['contentId', 'mood'], { unique: true })
+@Index('IDX_SAVED_CONTENTS_UNIQUE', ['contentId', 'mood', 'userId'], {
+  unique: true,
+})
 export class SavedContent {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ name: 'content_id' })
   contentId: number;
+
+  @Column({ name: 'user_id' })
+  userId: number;
 
   @Column({ length: 50 })
   mood: string;
@@ -30,4 +36,8 @@ export class SavedContent {
   @ManyToOne(() => ContentEntity, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'content_id' })
   content: ContentEntity;
+
+  @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
 }
