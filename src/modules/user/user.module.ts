6df4ef79a188ac2157bbbd349dc4e7408c29dbf5
Biglens <adedejibioku@gmail.com>
2025-07-22 19:24:36 +0100

    chore: register service and controller

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
+import { UserPreferencesModule } from '../user-preferences/user-preferences.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), UserPreferencesModule],
   providers: [UserService],
   exports: [UserService],
 })
