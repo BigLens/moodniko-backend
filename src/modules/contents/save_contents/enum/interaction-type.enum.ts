a618cdc3eb35912a0cb11a7f5349d46c1dfb62b5
Biglens <adedejibioku@gmail.com>
2025-10-04 16:38:06 +0100

    ref: separate interaction type enum from the entity

---
+++ src/modules/contents/save_contents/enum/interaction-type.enum.ts
@@ -0,0 +1,10 @@
+export enum InteractionType {
+  LIKE = 'like',
+  DISLIKE = 'dislike',
+  SAVE = 'save',
+  SHARE = 'share',
+  SKIP = 'skip',
+  PLAY = 'play',
+  COMPLETE = 'complete',
+  RATE = 'rate',
+}
